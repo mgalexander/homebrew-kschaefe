12937890e47b060a1ae5cff8612bbc22a98a2d08
Karl Schaefer <karlgschaefer@gmail.com>
2017-01-18 11:15:04 -0500

    Update dependencies and use sha256.

--- ant-git-tasks.rb
+++ ant-git-tasks.rb
@@ -10,22 +10,22 @@ class AntGitTasks < Formula
   depends_on "ant"
 
   resource "jsch" do
-    url "http://repo1.maven.org/maven2/com/jcraft/jsch/0.1.50/jsch-0.1.50.jar"
-    sha1 "fae4a0b1f2a96cb8f58f38da2650814c991cea01"
+    url "http://repo1.maven.org/maven2/com/jcraft/jsch/0.1.54/jsch-0.1.54.jar"
+    sha256 "92eb273a3316762478fdd4fe03a0ce1842c56f496c9c12fe1235db80450e1fdb"
   end
 
   resource "jgit" do
-    url "http://repo1.maven.org/maven2/org/eclipse/jgit/org.eclipse.jgit/3.0.0.201306101825-r/org.eclipse.jgit-3.0.0.201306101825-r.jar"
-    sha1 "c79117e7b178df4483d3f5f0bbfe961bb436697b"
+    url "http://repo1.maven.org/maven2/org/eclipse/org.eclipse.jgit/org.eclipse.jgit/4.6.0.201612231935-r/org.eclipse.jgit-4.6.0.201612231935-r.jar"
+    sha256 "64b6ff5b85947191ae028a85fdc782fe516b9343c3232a2e9a0e634bd6eab853"
   end
 
   resource "jgit-ant" do
-    url "http://repo1.maven.org/maven2/org/eclipse/jgit/org.eclipse.jgit.ant/3.0.0.201306101825-r/org.eclipse.jgit.ant-3.0.0.201306101825-r.jar"
-    sha1 "9d81e64a8211ee9a9dda32502a7a6a8cd28c4d30"
+    url "http://repo1.maven.org/maven2/org/eclipse/org.eclipse.jgit/org.eclipse.jgit.ant/4.6.0.201612231935-r/org.eclipse.jgit.ant-4.6.0.201612231935-r.jar"
+    sha256 "85246e9de0e1bd07cf1bd36584fe1b7d5e2f4721fdd290e725204486438e2a46"
   end
 
   def install
-    (share+"ant").install "ant-git-tasks-0.0.1.jar"
+    (share+"ant").install "ant-git-tasks-1.3.2.jar"
 
     resource("jsch").stage do
       (share+"ant").install Dir["*.jar"]
